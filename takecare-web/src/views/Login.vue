803bbf74f24f3a7133a4e09c7062aaae7a60021c
Esteban <goldmuros@yahoo.com>
2019-01-01 10:49:47 -0300

    Layout basico para login sin funcionalidad

---
+++ takecare-web/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-card-text>
+                <v-form ref="form" lazy-validation>
+                  <v-text-field id="name" v-model="name" prepend-icon="person" name="name" 
+                                label="Nombre" type="text" :rules="rules.nameRules"></v-text-field>
+                  <v-text-field id="password" v-model="password" prepend-icon="lock" name="password" 
+                                label="Clave" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="login">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      rules: {
+        nameRules: [
+          v => !!v || 'Nombre requerido',
+          v => v.length > 3 || 'El Nombre debe tener 4 o más carácteres'
+        ]
+      },
+      name: '',
+      password: null
+    }),
+    methods: {
+      login() {
+        if (this.$refs.form.validate()){
+          alert('hola')
+        } else {
+          alert('Algo salio mal')
+        }
+      }
+    }
+  }
+</script>
---
+++ takecare-web/src/views/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div mt-0>
+    <v-container fluid class="grid-list-md text-xs-center" v-if="!isMobile">
+      <v-layout row wrap>
+        
+      </v-layout>
+    </v-container>
+
+    <v-container fluid class="grid-list-xs grid-list-sm text-xs-center" v-if="isMobile">
+      <v-layout row wrap>
+        
+      </v-layout>
+    </v-container>
+  </div>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isMobile: false
+    }
+  },
+  created () {
+    let mediaQuery = window.matchMedia('(max-width: 959px)')
+
+    if (mediaQuery.matches) {
+      this.isMobile = true
+    }
+  }  
+}
+</script>
+
+<style>
+
+</style>
